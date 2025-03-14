7af6e9abad6f04dd028c5c70f9efc2a5c107f80e
ajorgense1-chwy <ajorgense1@chewy.com>
2025-03-13 19:56:50 -0700

    mar 13, 2025

--- cs5610_project2_spr25/src/main.jsx
+++ cs5610_project2_spr25/src/main.jsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import TicTacToe from './TicTacToe.jsx'
 import TicTacToeProvider from './TicTacToeProvider.jsx'
+import CheckBoxes from './CheckBoxes.jsx'
+import CheckApp from './CheckApp.jsx'
+import CheckAppProvider from './CheckAppProvider.jsx'
 
 //  return App()
 
+    {/* <TicTacToeProvider>
+      <TicTacToe />
+    </TicTacToeProvider> */}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TicTacToeProvider>
-      <TicTacToe />
-    </TicTacToeProvider>
+    <CheckAppProvider>
+      <CheckApp />
+    </CheckAppProvider>
+
   </StrictMode>,
 )
